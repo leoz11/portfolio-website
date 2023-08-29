b1699d75b31693def5943b0d106188416e2f93b5
leonardo <106268245+leoz11@users.noreply.github.com>
2023-08-29 19:12:46 -0300

    Updating files

--- src/App.js
+++ src/App.js
@@ -1,59 +1,142 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import { FaTwitter, FaLinkedin, FaGithub, FaYoutube } from 'react-icons/fa';
+import coffee from './coffee.gif';
+import { FaTwitter, FaLinkedin, FaGithub, FaYoutube, FaHtml5, FaCss3, FaJs, FaReact, FaBootstrap, FaNodeJs, FaDatabase, FaFire, FaGit } from 'react-icons/fa';
+import { BiLogoTailwindCss } from 'react-icons/bi';
+
+import background from './mini-blog.jpg'
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <div className="section" id="home">
-      <h1 className='title'><span className="angle-brackets">&lt;</span>LEONARDO OLIVEIRA<span className="angle-brackets">&gt;</span></h1>
-        <h1><i>Desenvolvedor Frontend</i></h1>
-        <p>Sou fascinado em desenvolver interfaces intuitivas e responsivas, utilizando: <br />
-          <b>HTML5</b>, <b>CSS3</b>, <b>ReactJS</b> e <b>Bootstrap</b>, tendo conhecimentos também com<br />
-          <b>NodeJS</b>, <b>MongoDB</b> e <b>GIT</b>. Estou sempre disposto e pronto para evoluir,<br />
-          conhecer novas tecnologias e aplicá-las em meus projetos.
-        </p>
-        <div className="social-icons">
-          <a href="https://twitter.com/your_twitter" target="_blank" rel="noopener noreferrer">
-            <FaTwitter className='twitter' />
-          </a>
-          <a href="https://www.linkedin.com/in/your_linkedin" target="_blank" rel="noopener noreferrer">
-            <FaLinkedin className='linkedin' />
-          </a>
-          <a href="https://github.com/your_github" target="_blank" rel="noopener noreferrer">
-            <FaGithub className='github' />
-          </a>
-          <a href="https://www.youtube.com/your_youtube" target="_blank" rel="noopener noreferrer">
-            <FaYoutube className='youtube' />
-          </a>
+      <div className='geral'>
+        {/* Seção Inicial */}
+        <div className="section" id="home">
+          <div>
+            <img src={coffee} alt="" />
+          </div>
+          <div className='title'>LEONARDO OLIVEIRA</div>
+          <i className='sub-title'>Desenvolvedor Frontend</i>
+          <p className='home-text'>
+            Sou fascinado em desenvolver interfaces intuitivas e responsivas, utilizando: <b>HTML5</b>, <b>CSS3</b>, <b>ReactJS</b>, <b>Tailwind</b> & <b>Bootstrap</b>, tendo conhecimentos também com <b>NodeJS</b>, <b>MongoDB</b>, <b>Firebase</b> e <b>GIT</b>. Estou sempre disposto e pronto para evoluir, conhecer novas tecnologias e aplicá-las em meus projetos.
+          </p>
+          <div className="social-icons">
+            <a href="https://twitter.com/your_twitter" target="_blank" rel="noopener noreferrer">
+              <FaTwitter className='twitter' />
+            </a>
+            <a href="https://www.linkedin.com/in/your_linkedin" target="_blank" rel="noopener noreferrer">
+              <FaLinkedin className='linkedin' />
+            </a>
+            <a href="https://github.com/your_github" target="_blank" rel="noopener noreferrer">
+              <FaGithub className='github' />
+            </a>
+            <a href="https://www.youtube.com/your_youtube" target="_blank" rel="noopener noreferrer">
+              <FaYoutube className='youtube' />
+            </a>
+          </div>
         </div>
-      </div>
-      <div className="section" id="conhecimentos">
-        <h2>Habilidades</h2>
-        <p>Meu nome é Leonardo e sou um desenvolvedor web apaixonado por criar aplicações incríveis com tecnologias modernas.</p>
-      </div>
-      <div className="section" id="sobre">
-        <h2>Sobre</h2>
-        <p>Meu nome é Leonardo e sou um desenvolvedor web apaixonado por criar aplicações incríveis com tecnologias modernas.</p>
-      </div>
-      <div className="section" id="projetos">
-        <h2>Projetos</h2>
-        <ul>
-          <li>
-            <strong>Projeto 1:</strong> Site de e-commerce em React.js
-            <p>Um site de e-commerce com carrinho de compras e integração com API de pagamento.</p>
-          </li>
-          <li>
-            <strong>Projeto 2:</strong> Aplicativo de lista de tarefas em React Native
-            <p>Um aplicativo mobile para gerenciar suas tarefas diárias de forma simples e intuitiva.</p>
-          </li>
-        </ul>
-      </div>
-      <div className="section" id="contato">
-        <h2>Contato</h2>
-        <p>Você pode entrar em contato comigo através do email: leonardo@example.com</p>
+        
+        {/* Seção de Projetos */}
+        <div className="section" id="projetos">
+          <div className="cards-container">
+            <div className="card">
+              <h3>Mini-Blog</h3>
+              <img className='background' src={background} alt="" />
+              <p>Projeto "Mini-Blog" feito utilizando REACT.JS e Firebase.</p>
+              <a href="URL_DO_PROJETO" target="_blank" rel="noopener noreferrer" className="visit-button">Visitar</a>
+            </div>
+            {/* Projeto 2 */}
+            <div className="card">
+              <h3>Projeto 2</h3>
+              <p>Descrição do Projeto 2</p>
+              <a href="URL_DO_PROJETO" target="_blank" rel="noopener noreferrer" className="visit-button">Visitar</a>
+            </div>
+            {/* Projeto 3 */}
+            <div className="card">
+              <h3>Projeto 3</h3>
+              <p>Descrição do Projeto 3</p>
+              <a href="URL_DO_PROJETO" target="_blank" rel="noopener noreferrer" className="visit-button">Visitar</a>
+            </div>
+          </div>
+          <div className="icon-container">
+            <div className="tech-card">
+              <div className="tech-icon">
+                <FaHtml5 className="html-icon" />
+              </div>
+              <div>HTML</div>
+            </div>
+            <div className="tech-card">
+              <div className="tech-icon">
+                <FaCss3 className="css-icon" />
+              </div>
+              <div>CSS</div>
+            </div>
+            <div className="tech-card">
+              <div className="tech-icon">
+                <FaJs className="js-icon" />
+              </div>
+              <div>JavaScript</div>
+            </div>
+            <div className="tech-card">
+              <div className="tech-icon">
+                <FaReact className="react-icon" />
+              </div>
+              <div>React</div>
+            </div>
+            <div className="tech-card">
+              <div className="tech-icon">
+                <BiLogoTailwindCss className="tailwind-icon" />
+              </div>
+              <div>Tailwind</div>
+            </div>
+        </div>
+        <div className="icon-container">
+        <div className="tech-card">
+              <div className="tech-icon">
+                <FaBootstrap className="bootstrap-icon" />
+              </div>
+              <div>Bootstrap</div>
+            </div>
+            <div className="tech-card">
+              <div className="tech-icon">
+                <FaNodeJs className="node-icon" />
+              </div>
+              <div>Node.js</div>
+            </div>
+            <div className="tech-card">
+              <div className="tech-icon">
+                <FaDatabase className="db-icon" />
+              </div>
+              <div>Database</div>
+            </div>
+            <div className="tech-card">
+              <div className="tech-icon">
+                <FaFire className="firebase-icon" />
+              </div>
+              <div>Firebase</div>
+            </div>
+            <div className="tech-card">
+              <div className="tech-icon">
+                <FaGit className="git-icon" />
+              </div>
+              <div>Git</div>
+            </div>
+          </div>
+        </div>
+
+        {/* Seção Sobre */}
+        <div className="section" id="sobre">
+          <h2>Sobre</h2>
+          <p>Meu nome é Leonardo e sou um desenvolvedor web apaixonado por criar aplicações incríveis com tecnologias modernas.</p>
+        </div>
+
+        {/* Seção de Contato */}
+        <div className="section" id="contato">
+          <h2>Contato</h2>
+          <p>Você pode entrar em contato comigo através do email: leonardo@example.com</p>
+        </div>
       </div>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,102 +1,57 @@
-// Navbar.js
-
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-scroll';
 import './Navbar.css';
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [scrollProgress, setScrollProgress] = useState(0);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [scrollProgress, setScrollProgress] = useState(0);
 
   useEffect(() => {
     const updateScrollProgress = () => {
-      const totalHeight = document.documentElement.scrollHeight - window.innerHeight;
       const scrolledHeight = window.scrollY;
+      const totalHeight = document.documentElement.scrollHeight - window.innerHeight;
       const progress = (scrolledHeight / totalHeight) * 100;
       setScrollProgress(progress);
     };
 
     window.addEventListener('scroll', updateScrollProgress);
-
-    return () => {
-      window.removeEventListener('scroll', updateScrollProgress);
-    };
+    return () => window.removeEventListener('scroll', updateScrollProgress);
   }, []);
 
   const toggleNavbar = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+
+  const sections = [
+    { id: 'home', text: 'Home' },
+    { id: 'projetos', text: 'Projetos' },
+    { id: 'contato', text: 'Contato' },
+  ];
 
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <div className="brand">Leonardo Oliveira</div>
+      <div className="brand">&lt; Léo &gt;</div>
         <div className={`navbar-menu ${isMenuOpen ? 'active' : ''}`}>
-          <Link
-            activeClass="active"
-            to="home"
-            spy={true}
-            smooth={true}
-            offset={-70}
-            duration={500}
-            onClick={toggleNavbar}
-          >
-            Home
-          </Link>
-          <Link
-            activeClass="active"
-            to="conhecimentos"
-            spy={true}
-            smooth={true}
-            offset={-70}
-            duration={500}
-            onClick={toggleNavbar}
-          >
-            Conhecimentos
-          </Link>
-          <Link
-            activeClass="active"
-            to="sobre"
-            spy={true}
-            smooth={true}
-            offset={-70}
-            duration={500}
-            onClick={toggleNavbar}
-          >
-            Sobre
-          </Link>
-          <Link
-            activeClass="active"
-            to="projetos"
-            spy={true}
-            smooth={true}
-            offset={-70}
-            duration={500}
-            onClick={toggleNavbar}
-          >
-            Projetos
-          </Link>
-          <Link
-            activeClass="active"
-            to="contato"
-            spy={true}
-            smooth={true}
-            offset={-70}
-            duration={500}
-            onClick={toggleNavbar}
-          >
-            Contato
-          </Link>
+          {sections.map((section) => (
+            <Link
+              key={section.id}
+              activeClass="active"
+              to={section.id}
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              onClick={toggleNavbar}
+            >
+              {section.text}
+            </Link>
+          ))}
         </div>
-        <div className={`navbar-toggle ${isMenuOpen ? 'active' : ''}`} onClick={toggleMenu}>
-          <span className={isMenuOpen ? 'cross' : ''}></span>
-          <span className={isMenuOpen ? 'cross' : ''}></span>
-          <span className={isMenuOpen ? 'cross' : ''}></span>
+        <div className={`navbar-toggle ${isMenuOpen ? 'active' : ''}`} onClick={toggleNavbar}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
         </div>
       </div>
       <div className="progress-bar" style={{ width: `${scrollProgress}%` }} />
