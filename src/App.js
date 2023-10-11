7a3cb1967838f29d3a2e12af73124f20cfd34be4
leonardo <106268245+leoz11@users.noreply.github.com>
2023-10-11 14:02:16 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -49,15 +49,15 @@ const App = () => {
             <div className='projects'>
               <div className="card">
                 <h3>Mini-Blog</h3>
-                <img className='background' src={background} alt="" />
+                <img className='background' src={background} alt="miniblog" />
                 <p>Projeto "Mini-Blog" feito utilizando REACT.JS e Firebase.</p>
                 <a href="https://mini-blog-leo.vercel.app" target="_blank" rel="noopener noreferrer" className="visit-button">Visitar</a>
               </div>
             </div>
             <div className='projects'>
               <div className="card">
-                <h3>Mini-Blog</h3>
-                <img className='background' src={background_two} alt="" />
+                <h3>Countdown to next year</h3>
+                <img className='background' src={background_two} alt="countdown" />
                 <p>Projeto "Countdown to next year" feito utilizando REACT.JS.</p>
                 <a href="https://mini-blog-leo.vercel.app" target="_blank" rel="noopener noreferrer" className="visit-button">Visitar</a>
               </div>
